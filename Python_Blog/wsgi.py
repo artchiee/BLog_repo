f3e93c4754d7adb580917cce3cf632ac1410d688
Nouamanezh909 <nouamanezh99@gmail.com>
2019-10-21 18:37:00 +0100

    Commiting initial

--- Python_Blog/wsgi.py
+++ Python_Blog/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Python_Blog.settings')
 
 application = get_wsgi_application()
+
+# using white noise to serve static files
+application = DjangoWhiteNoise(application)
